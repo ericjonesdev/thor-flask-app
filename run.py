4f760160de4dffe5e6facf0fa14accaab462caa9
ericjonesdev <ericjones2004@yahoo.com>
2023-02-05 21:31:10 +0000

    Add functionality for flask app to run

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 
@@ -7,4 +8,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello, World"
-    
+    if __name__ == "__main__":
+        app.run(
+            host=os.environ.get("IP", "0.0.0.0"),
+            port=int(os.environ.get("PORT", "5000")),
+            debug=True)
