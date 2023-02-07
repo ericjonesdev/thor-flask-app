0f653afaf49b7da7b40228f0440099fd6eb0a414
ericjonesdev <ericjones2004@yahoo.com>
2023-02-07 21:32:17 +0000

    Add data.json file with associated elements to render list of characters on about page

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template
 
 
@@ -7,22 +8,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", page_title="Home")
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data= []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
